a40d29aafbf97cb79d44149756a34133576e9373
James Karlinski <89712334+jamesk08@users.noreply.github.com>
2021-08-29 12:24:12 -0400

    moving author comments to top

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,12 +1,12 @@
+/*
+ *  UCF COP3330 Fall 2021 Assignment 1 Solution
+ *  Copyright 2021 James Karlinski
+ */
 package org.example;
 
 import java.text.MessageFormat;
 import java.util.Scanner;
 
-/*
- *  UCF COP3330 Fall 2021 Assignment 1 Solution
- *  Copyright 2021 James Karlinski
- */
 public class App
 {
     public static void main( String[] args )
